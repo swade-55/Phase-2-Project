2679d927e32d6619af7f348f30621719e08348d2
Samuel Wade <75805828+swade-55@users.noreply.github.com>
2023-06-12 16:48:54 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div id="about">
             <h2>Labor Capacity Planner</h2>
-            <p>This tool helps wholesale grocery warehouse management by enabling them to upload employees with their own specific productivity metrics and updates the summary to reflect potential gaps. </p>
+            <p>This tool helps wholesale grocery warehouse management by enabling them to upload employees with their own specific productivity metrics and update the summary to reflect potential gaps. </p>
             <p>Examples of some productivity metrics would consist of uptime(the total length of time the associate was engaged in their primary job function), units per hour(total units that associate has selected per hour), attendance(number of days they attended work divided by number of days they were scheduled multiplied by 100). Each associate would have their individual variations of the above metrics displayed as part of their associate card. Wrapping all of it together, each metric would be totalled or averaged based on the relevance to each metric and then displayed towards the top of the spreadsheet. I like to call this section the "Executive Summary" tab. </p>
             <div>
                 <h3>Sam Wade Links</h3>
